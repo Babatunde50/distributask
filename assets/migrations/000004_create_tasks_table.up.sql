3968d623cb541620a23216d56c629bffa57b003e
Babatunde Ololade <babatundeololade50@gmail.com>
2023-05-11 02:15:52 +0300

    update task migration file

--- assets/migrations/000004_create_tasks_table.up.sql
+++ assets/migrations/000004_create_tasks_table.up.sql
@@ -4,26 +4,20 @@ CREATE TYPE task_status AS ENUM (
     'completed',
     'failed'
 );
-CREATE TYPE task_type AS ENUM (
-    'image_processing',
-    'data_processing',
-    'video_processing',
-    'audio_processing',
-    'text_processing',
-    'other'
-);
+CREATE TYPE task_type AS ENUM ('image_processing');
+
+
 CREATE TABLE tasks (
     id SERIAL NOT NULL PRIMARY KEY,
     type task_type NOT NULL,
     payload JSONB NOT NULL,
-    priority INTEGER NOT NULL DEFAULT 0,
+    priority INTEGER NOT NULL DEFAULT 1,
     status task_status NOT NULL DEFAULT 'queued',
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    timeout INTEGER NOT NULL DEFAULT 0,
+    timeout INTEGER NOT NULL DEFAULT 30,
     retry_count INTEGER NOT NULL DEFAULT 0,
-    max_retries INTEGER NOT NULL DEFAULT 0,
-    next_retry_at timestamp(0) with time zone,
+    max_retries INTEGER NOT NULL DEFAULT 5,
     user_id INTEGER NOT NULL REFERENCES users(id),
-    result TEXT
+    result TEXT NOT NULL DEFAULT '-'
 );
